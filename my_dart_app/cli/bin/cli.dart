02666c266d870ac2d73b8910651e0968b2cd6159
Erkan3034 <turguterkan55@gmail.com>
2026-03-05 18:47:20 +0300

    added cli searchWiki

--- my_dart_app/cli/bin/cli.dart
+++ my_dart_app/cli/bin/cli.dart
@@ -1,11 +1,19 @@
 const version = '0.0.1'; 
 const description = 'A simple CLI application for Dart by MrCoder.';
 void main(List<String> arguments) {
-  if (arguments.isEmpty || arguments.contains('help')) {
+  if (arguments.isEmpty || arguments.first=='help') {
     printUsage();
-  } else if (arguments.contains('description')) {
+  } 
+  
+  else if (arguments.contains('description')) {
     print('My Dart App CLI version $description');
   }
+  
+  else if (arguments.first == 'search') {
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    searchWikipedia(inputArgs);
+  }
+  
   else{
     printUsage();
   }
@@ -14,4 +22,8 @@ void main(List<String> arguments) {
 
 void printUsage(){
   print("The following commands are valid: 'description', 'version', 'search <ARTICLE-TITLE>'");
+}
+
+ void searchWikipedia(List<String>? args){
+  print("searchWikipedia received arguments : $args");
 }

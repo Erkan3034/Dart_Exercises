17b24a181fc217ac71937bb4fd485dadb9eb61fb
Erkan3034 <turguterkan55@gmail.com>
2026-03-12 15:41:40 +0300

    Add command_runner dependency and implement rpgGame functionality

--- dartpedia/cli/bin/cli.dart
+++ dartpedia/cli/bin/cli.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:command_runner/command_runner.dart';
+
 const version = '0.0.1'; 
 const description = 'A simple CLI application for Dart by MrCoder.';
 void main(List<String> arguments) {
